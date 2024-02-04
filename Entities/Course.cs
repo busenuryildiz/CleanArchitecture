6ce34cba8a280158f65315b9ac5f8e394d1b078d
busenuryildiz <busenuryildiz5337@gmail.com>
2024-02-05 00:16:41 +0300

    .

--- Entities/Course.cs
+++ Entities/Course.cs
@@ -8,5 +8,12 @@ namespace IntroCQRS.Entities
 {
     public class Course
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Instructor { get; set; }
+        public int Credit { get; set; }
+        public int Capacity { get; set; }
+        public int Enrolled { get; set; }
     }
+
 }
