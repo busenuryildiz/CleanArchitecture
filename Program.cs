cfafd3b336f470113a673affebbf5577164b6436
busenuryildiz <busenuryildiz5337@gmail.com>
2024-02-05 00:13:36 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string message = "Krediler";
+int term = 12;
+double amount = 100000;
+
+//variables camelCase
+bool isAuthenticated = false;
+Console.WriteLine(message);
